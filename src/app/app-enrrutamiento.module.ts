5f67327a40662b67519d5e076bfa706839683438
Carlos Vala <cavallenas@gmail.com>
2020-04-13 22:22:18 -0500

    crear componentes,fin de seccion 3 y crear rutas para nuestra paginas, uso de hash

---
+++ src/app/app-enrrutamiento.module.ts
@@ -0,0 +1,29 @@
+import { NgModule, Component } from '@angular/core';
+
+import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
+import { ArticulosComponent } from './pages/articulos/articulos.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+
+const app_routes: Routes = [
+    {path: 'inicio', component: PortafolioComponent},
+    {path: 'articulos', component: ArticulosComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'acerca-de', component: AcercaDeComponent},
+    {path: 'contacto', component: ContactoComponent},
+    {path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+];
+
+@NgModule({
+
+    imports: [
+        RouterModule.forRoot(app_routes, { useHash: true})
+    ],
+    exports: [
+        RouterModule
+    ]
+
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// rutas
+import { AppRoutingModule } from './app-enrrutamiento.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
+import { ArticulosComponent } from './pages/articulos/articulos.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    ArticulosComponent,
+    AcercaDeComponent,
+    BlogComponent,
+    ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
