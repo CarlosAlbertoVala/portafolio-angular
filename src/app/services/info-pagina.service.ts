e43189a6f5150649c7c8933a3b58786edf00b1a5
Carlos Vala <cavallenas@gmail.com>
2020-04-14 23:49:03 -0500

    cargar la informacion de la nube firebase a nuestro info-pagina.service a nuestra pagina

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,29 @@ export class InfoPaginaService {
 
     info: InfoPagina = {};
     cargada = false;
+    equipo: any[] = [];
   constructor(private http: HttpClient) {
     // console.log('servicio de infoPagina listo carlos');
-
-    // leer el archivo  json para que lea nuestra pagina
-    this.http.get('assets/data/data-pagina.json') // es la definicion donde esta el archivo
-      .subscribe( (resp: InfoPagina) => {  // es para ejecutar la repuestas
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);  // con esto te muestra todo el contenido
-  
-      });
+    this.cargarInfo();
+    this.cargarEquipo();
   }
+
+  private cargarInfo(){
+       // leer el archivo  json para que lea nuestra pagina
+    this.http.get('assets/data/data-pagina.json') // es la definicion donde esta el archivo
+    .subscribe( (resp: InfoPagina) => {  // es para ejecutar la repuestas
+      this.cargada = true;
+      this.info = resp;
+      // console.log(resp);  // con esto te muestra todo el contenido
+    });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-40be7.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+      console.log(resp);
+    });
+  }
+
 }
