75cfd516a9aed0e8ea5e8748c6fd999fb778da6f
Carlos Vala <cavallenas@gmail.com>
2020-04-15 22:09:57 -0500

    creando producto servicio y su intrface

---
+++ src/app/interfaces/producto.interface.ts
@@ -0,0 +1,6 @@
+export interface interfaceProducto {
+    categoria: string;
+    cod: string;
+    titulo: string;
+    url: string;
+  }
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { interfaceProducto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+
+
+  constructor(private http: HttpClient) {
+    this.cargarProductos();
+   }
+
+   private cargarProductos(){
+    this.http.get('https://angular-html-40be7.firebaseio.com/productos_idx.json')
+      .subscribe((resp: interfaceProducto[]) => {
+
+        console.log(resp);
+
+        this.cargando = false;
+      });
+
+   }
+
+
+}
